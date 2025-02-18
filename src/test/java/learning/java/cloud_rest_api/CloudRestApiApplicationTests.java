5f047a89797c65e94ba85754cac71f1d3d749e6c
Vilson Luiz Dalle Mole <vldmole@gmail.com>
2025-02-18 16:00:55 -0300

    Initial commit

---
+++ src/test/java/learning/java/cloud_rest_api/CloudRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.java.cloud_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
